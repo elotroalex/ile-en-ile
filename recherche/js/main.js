2295337120c0eaa5ffc09758070f2a3dc34817b0
Alex Gil <colibri.alex@gmail.com>
2021-10-20 14:50:17 -0400

    added spinner

--- recherche/js/main.js
+++ recherche/js/main.js
@@ -15,6 +15,32 @@ async function beforeUnloadListener(event) {
   search.value = "";
 }
 
+// Spinner
+
+var opts = {
+  lines: 13, // The number of lines to draw
+  length: 38, // The length of each line
+  width: 17, // The line thickness
+  radius: 35, // The radius of the inner circle
+  scale: 1, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  speed: 1, // Rounds per second
+  rotate: 0, // The rotation offset
+  animation: "spinner-line-fade-quick", // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: "#7ab1d2", // CSS color or array of colors
+  fadeColor: "transparent", // CSS color or array of colors
+  top: "75%", // Top position relative to parent
+  left: "50%", // Left position relative to parent
+  shadow: "0 0 1px transparent", // Box-shadow for the lines
+  zIndex: 2000000000, // The z-index (defaults to 2e9)
+  className: "spinner", // The CSS class to assign to the spinner
+  position: "absolute", // Element positioning
+};
+
+var target = document.getElementById("spin-box");
+// var spinner = new Spin.Spinner(opts).spin(target);
+
 // Search the list
 
 async function searchList(searchText) {
@@ -22,14 +48,15 @@ async function searchList(searchText) {
   let posts = await raw.json();
   let options = {
     includeMatches: true,
-    threshold: 0.4,
-    keys: ["Content", "Title"],
+    threshold: 0.3,
+    keys: ["Content"],
   };
-  const myIndex = Fuse.createIndex(options.keys, posts)
-  
+
+  const myIndex = Fuse.createIndex(options.keys, posts);
+
   let f = new Fuse(posts, options, myIndex);
   let allResults = f.search(searchText);
-  let topTenResults = allResults.slice(0,9);
+  let topTenResults = allResults.slice(0, 9);
 
   if (searchText.length == 0) {
     matchList.innerHTML = "";
